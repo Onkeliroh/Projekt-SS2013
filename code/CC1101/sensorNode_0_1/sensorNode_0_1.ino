450238a222610cfba742ed5088ac6163bc334299
jennycgonzalez <jennycgonzalez@gmail.com>
2013-08-15 16:25:57 +0200

    All sketches were refactored, new classes were created

---
+++ code/CC1101/sensorNode_0_1/sensorNode_0_1.ino
@@ -0,0 +1,103 @@
+#include "EEPROM.h"
+#include "analogComp.h"
+#include "ccSensorNode.h"
+#include "accel.h"
+
+#define SENSOR_NODE_ID 3
+#define TWIN_NODE_ID 2
+
+#define enableRFChipInterrupt()     attachInterrupt(0, RFChipInterrupt, FALLING);
+#define disableRFChipInterrupt()    detachInterrupt(0);
+
+#define enableaLowBattInterrupt()     analogComparator.enableInterrupt(lowBattInterrupt, FALLING);
+#define disableLowBattInterrupt()     analogComparator.disableInterrupt();
+
+/////////////////////
+//--- INSTANCES ---//
+/////////////////////
+
+CCSENSORNODE _sensorNode = CCSENSORNODE(SENSOR_NODE_ID, TWIN_NODE_ID);
+ACCEL _accel = ACCEL();
+
+///////////////////
+//--- MEMBERS ---//
+///////////////////
+
+boolean _packetAvailable = false;
+
+boolean _batteryIsLow = false;
+
+//////////////////////
+//--- INTERRUPTS ---//
+//////////////////////
+
+// Handle interrupt from CC1101 (INT0)
+void RFChipInterrupt()
+{
+    _packetAvailable = true;            // set the flag thar a package is available
+   
+}
+
+// Handle interrupt from Analog Comparator 
+void lowBattInterrupt(void)
+{
+    _batteryIsLow = true;  
+}
+
+
+///////////////////////////
+//--- INITIALIZATIONS ---//
+///////////////////////////
+
+
+void setup()
+{
+    _sensorNode.setup();
+    enableaLowBattInterrupt();
+    enableRFChipInterrupt(); 
+}
+
+
+/////////////////////
+//--- MAIN LOOP ---//
+/////////////////////
+
+// The loop method gets called on and on after the start of the system.
+void loop()
+{
+  
+//    if(_batteryIsLow)
+//    {
+//        _sensorNode.reportLowBatt(); 
+//        disableLowBattInterrupt();         
+//    }
+    
+    if(_packetAvailable)
+    {
+        disableRFChipInterrupt();
+        
+        if(_sensorNode.ccReceive())
+        {
+            //_sensorNode.ccPrintPacket();
+            _sensorNode.ccHandle();  
+        }
+        else
+        {
+            if(!_sensorNode.bubbleIsSender())
+                _sensorNode.reportRSSI();          
+        }
+        
+        _packetAvailable = false;   
+        enableRFChipInterrupt();  
+    }
+    else 
+    {
+        _sensorNode.ledBlink();
+        delay(100);
+    }
+  
+    if(_accel.wasShaken())
+        _sensorNode.reportAccelEvent();  
+
+}
+
