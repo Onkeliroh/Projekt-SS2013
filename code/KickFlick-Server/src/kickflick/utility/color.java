ee9aea59e0a03f031be835dc26cbb392fa9333dd
onkeliroh <onkeliroh@gmx.de>
2013-10-01 16:33:05 +0200

    added new color values

--- code/KickFlick-Server/src/kickflick/utility/color.java
+++ code/KickFlick-Server/src/kickflick/utility/color.java
@@ -5,22 +5,22 @@ import java.util.Enumeration;
 // Color Enumeration
 // compare with Leds.cpp for more color values or to change them
 public enum color {
-    GREEN("GREEN", (byte)0),
-    LEMON_GREEN("Lemon Green", (byte)1),
-    RED_DIM("Red (dim)", (byte)2),
-    RED("Red", (byte) 3),
-    RED_BRIGHT("Red (bright)", (byte) 4),
-    BLUE_DIM("Blue (dim)", (byte) 5),
-    BLUE("Blue", (byte) 6),
+    WHITE("White", (byte) 0),
+    RED_BRIGHT("Red (bright)", (byte) 1),
+    RED("Red", (byte) 2),
+    RED_DIM("Red (dim)", (byte) 3),
+    GREEN_BRIGHT("Green (bright)", (byte) 4),
+    GREEN("Green", (byte) 5),
+    GREEN_DIM("Green (dim)", (byte) 6),
     BLUE_BRIGHT("Blue (bright)", (byte) 7),
-    YELLOW_DIM("Yellow (dim)", (byte) 8),
-    YELLOW("Yellow", (byte) 9),
+    BLUE("Blue", (byte) 8),
+    BLUE_DIM("Blue (dim)", (byte) 9),
     YELLOW_BRIGHT("Yellow (bright)", (byte) 10),
-    PINK("PINK", (byte)11),          //TODO change to real value
-    ORANGE("ORANGE", (byte)12),
-    WHITE("WHITE", (byte)13),
-    FUCHSIA("FUCHSIA", (byte)14),
-    TURQUOISE("TURQUOISE", (byte)15),
+    YELLOW("Yellow", (byte) 11),
+    YELLOW_DIM("Yellow (dim)", (byte) 12),
+    LILA("Lila", (byte) 13),
+    ORANGE("Orange", (byte) 14),
+    TURQUOISE("Turquoise", (byte)15),
     BLACK("Black",(byte)16);
 
     private final String colorName;
