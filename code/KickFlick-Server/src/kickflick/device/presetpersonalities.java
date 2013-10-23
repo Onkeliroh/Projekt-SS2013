b0c4e73f9656513dab9d9f2fdeedd70e746e8fbd
ischonger <ischonger@gmail.com>
2013-10-23 18:01:27 +0200

    add tanja and mama

--- code/KickFlick-Server/src/kickflick/device/presetpersonalities.java
+++ code/KickFlick-Server/src/kickflick/device/presetpersonalities.java
@@ -1,55 +1,69 @@
 package kickflick.device;
 
+import kickflick.utility.DefaultHashMap;
 import kickflick.utility.color;
+import kickflick.utility.keys;
 import kickflick.utility.pattern;
 
 import java.util.HashMap;
 
 public enum presetpersonalities
 {
+    //TODO check for durrations
     Paul(new personality(
             "Paul",
             (short) 0,
-            new byte[]{color.BLUE.get_key(), color.RED.get_key(), color.GREEN.get_key(), color.ORANGE.get_key()},    //Color 1
-            new byte[]{color.BLUE.get_key(), color.RED.get_key(), color.GREEN.get_key(), color.ORANGE.get_key()},    //Color 2
-            new byte[]{pattern.BLINK.get_key(), pattern.BLINK.get_key(), pattern.BLINK.get_key(), pattern.RAINBOW.get_key()},     //Pattern
-            new HashMap<String, byte[]>()
+            new DefaultHashMap<keys, reaction[]>(new reaction[]{
+                        new reaction(color.RED,color.RED,pattern.BLINK),
+                        new reaction(color.GREEN,color.GREEN,pattern.BLINK),
+                        new reaction(color.ORANGE,color.ORANGE,pattern.RAINBOW)
+                }),
+            new HashMap<String, reaction>()
             {
                 {
-                    put("Tanja", new byte[]{pattern.BLINK.get_key(), color.RED.get_key(), color.BLUE.get_key()});
-                    put("Mama", new byte[]{pattern.BLINK.get_key(), color.WHITE.get_key(), color.BLACK.get_key()});
+                    put("Tanja", new reaction(color.RED, color.BLUE, pattern.BLINK));
+                    put("Mama", new reaction(color.WHITE, color.BLACK, pattern.BLINK));
                 }
-            }
+            },
+            new reaction(color.BLUE,color.BLUE,pattern.BLINK, 10000) //standby
+
     )),
 
     Tanja(new personality(
             "Tanja",
-            (short) 0,
-            new byte[]{color.LILA.get_key(), color.YELLOW_DIM.get_key(), color.YELLOW_BRIGHT.get_key(), color.RED_BRIGHT.get_key()},
-            new byte[]{color.LILA.get_key(), color.WHITE.get_key(), color.YELLOW_BRIGHT.get_key(), color.RED_DIM.get_key()},
-            new byte[]{pattern.BLINK.get_key(), pattern.BLINK.get_key(), pattern.RAINBOW.get_key(), pattern.FADE.get_key()},
-            new HashMap<String, byte[]>()
-            {
-                {
-                    put("Paul", new byte[]{pattern.FADE.get_key(), color.RED.get_key(), color.RED_BRIGHT.get_key()});
-                    put("Mama", new byte[]{pattern.BLINK.get_key(), color.RED.get_key(), color.RED_BRIGHT.get_key()});
-                }
-            }
+            (short) 1,
+            new DefaultHashMap<keys, reaction[]>(new reaction[]{
+                new reaction(color.YELLOW_DIM,color.WHITE,pattern.BLINK),
+                new reaction(color.YELLOW_BRIGHT,color.YELLOW_BRIGHT,pattern.BLINK),
+                new reaction(color.RED,color.RED_BRIGHT,pattern.FADE)
+            }),
+        new HashMap<String, reaction>()
+    {
+        {
+            put("Paul", new reaction(color.RED, color.RED_BRIGHT, pattern.FADE));
+            put("Mama", new reaction(color.RED, color.RED_BRIGHT, pattern.BLINK));
+        }
+    },
+        new reaction(color.LILA,color.LILA,pattern.BLINK, 10000) //standby
     )),
+
 
     Mama(new personality(
             "Mama",
-            (short) 0,
-            new byte[]{color.BLUE_DIM.get_key(), color.ORANGE.get_key(), color.RED.get_key(), color.RED_BRIGHT.get_key()},
-            new byte[]{color.WHITE.get_key(), color.ORANGE.get_key(), color.RED_DIM.get_key(), color.RED.get_key()},
-            new byte[]{pattern.BLINK.get_key(), pattern.BLINK.get_key(), pattern.BLINK.get_key(), pattern.BLINK.get_key()},
-            new HashMap<String, byte[]>()
-            {
-                {
-                    put("Paul", new byte[]{pattern.BLINK.get_key(), color.GREEN.get_key(), color.ORANGE.get_key()});
-                    put("Tanja", new byte[]{pattern.FADE.get_key(), color.GREEN.get_key(), color.ORANGE.get_key()});
-                }
-            }
+            (short) 2,
+            new DefaultHashMap<keys, reaction[]>(new reaction[]{
+                new reaction(color.ORANGE,color.ORANGE,pattern.BLINK),
+                new reaction(color.RED,color.RED_DIM,pattern.BLINK),
+                new reaction(color.RED_BRIGHT,color.RED,pattern.RAINBOW)
+            }),
+        new HashMap<String, reaction>()
+    {
+        {
+            put("Paul", new reaction(color.GREEN, color.ORANGE, pattern.BLINK));
+            put("Mama", new reaction(color.GREEN, color.ORANGE, pattern.BLINK));
+        }
+    },
+        new reaction(color.BLUE_DIM,color.WHITE,pattern.BLINK, 10000) //standby
     ));
 
     private final personality personality_;
